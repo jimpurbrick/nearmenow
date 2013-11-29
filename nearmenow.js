70e7259ea6d8c85dffd001e87d7a1718f8ccad9e
Jim Purbrick <jimp@fb.com>
2013-11-29 11:42:41 +0000

    Formatting

--- nearmenow.js
+++ nearmenow.js
@@ -2,7 +2,14 @@
     
     function getMap()
     {
-        map = new Microsoft.Maps.Map(document.getElementById('myMap'), {credentials: 'Ajq-7x-U0c2VIR9WJxy3wXHPpR_ZwRdkjzT0PSvyhKah94KDRGk1FPkY85hiFW5U', showDashboard: false, enableSearchLogo: false, enableClickableLogo: false, mapTypeId : Microsoft.Maps.MapTypeId.road});
+        map = new Microsoft.Maps.Map(
+	    document.getElementById('myMap'), {
+		credentials: 'Ajq-7x-U0c2VIR9WJxy3wXHPpR_ZwRdkjzT0PSvyhKah94KDRGk1FPkY85hiFW5U', 
+		showDashboard: false, 
+		enableSearchLogo: false, 
+		enableClickableLogo: false, 
+		mapTypeId : Microsoft.Maps.MapTypeId.road
+	    });
         map.entities.clear();
         var geoLocationProvider = new Microsoft.Maps.GeoLocationProvider(map);
         geoLocationProvider.getCurrentPosition({ showAccuracyCircle: true, updateMapView: true });
@@ -10,9 +17,9 @@
     }   
     
     window.fbAsyncInit = function() {
-
+	
 	var map = null;
-
+	
         // init the FB JS SDK
         FB.init({
             appId      : '378209865646769',
@@ -23,7 +30,6 @@
 	// Login to FB and get event data.
         FB.login(function(response) {
             if (response.authResponse) {
-		console.log('Welcome!  Fetching your information.... ');
 		FB.api('/me?fields=events.until(1384977600).limit(25).fields(venue)', function(response) {
                     console.log("Events" + response.events);
 		});
@@ -31,10 +37,10 @@
 		console.log('User cancelled login or did not fully authorize.');
             }
         }, {scope: "user_events"});
-
+	
 	// Create the map.
 	map = getMap();
-
+	
 	// TODO: populate the map with FB data.
     };
     
