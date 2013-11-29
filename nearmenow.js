d3d6ffd3de5d23b6e3bd218c855a72b3f32165cd
Jim Purbrick <jimp@fb.com>
2013-11-29 16:14:55 +0000

    Fixes

--- nearmenow.js
+++ nearmenow.js
@@ -50,56 +50,54 @@
 	// Login to FB and get event data.
         FB.login(function(response) {
             if (response.authResponse) {
-		FB.api('/me?fields=events.since(1385564000).limit(25).fields(name,venue,cover,start_time)', function(response) {
-		    // Create the map.
-		    map = getMap();
-		   
-		    // Loop through events adding pushpins.
-		    for(var i = 0; i < response.events.data.length; ++i) {
-                        var event = response.events.data[i];
-                        try {
-		            setPushPin(map, event.start_time, event.name, event.cover.source, event.venue.latitude, event.venue.longitude);
-                        } catch (err) {}
-                    }
-                });
-                /*
-                FB.api('/me?fields=friends.limit(20).fields(checkins.limit(10).fields(created_time,coordinates),picture)', function(response) {
-                    map = getMap();
-                    // Loop through friends' checkins
-                    for (var i = 0; i < response.friends.data.length; i++) {
-                        try {
-                            for (var j = 0; j < response.friends.data[i].checkins.data; j++) {
-                                var coordinates = response.friends.data[i].checkins.data[j].coordinates;
-                                var createdTime = (new Date(response.friends.data[i].checkins.data[j].created_time)).getTime();
-
-                            }
-                        } catch (err) {}
-                    }
-                });*/
-                // Get check in data.
-                var xmlhttp = new XMLHttpRequest(); // TODO: IE5, IE6? ;-)
-                xmlhttp.onreadystatechange = function() {
-                    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-                        var response = xmlhttp.responseText.substr(9);
-                        var data = JSON.parse(response);
-                        map = getMap();
-                        for(var user in data) {
-                            //console.log(data[user].p_url);
-                            var userData = data[user];
-                            try {
-                                setPushPin(map, userData[time],
-                                           userData[p_name], userData[pic], userData[lat], userData[lon]);
-                            } catch (err) {}
-                        }
-                    }
-                };
-            } else {
-		console.log('User cancelled login or did not fully authorize.');
-            }
-        }, {scope: "user_events"});
-
-	xmlhttp.open("GET", "https://www.martinoluca.sb.facebook.com/nearmenow/friends/", true);
-	xmlhttp.send();
+		FB.api('/me?fields=events.since(1385564000).limit(25).fields(name,venue,cover,start_time)', 
+		       function(response) {
+			   // Create the map.
+			   map = getMap();
+			   
+			   // Loop through events adding pushpins.
+			   for(var i = 0; i < response.events.data.length; ++i) {
+                               var event = response.events.data[i];
+                               try {
+				   setPushPin(map, event.start_time, event.name, event.cover.source, event.venue.latitude, event.venue.longitude);
+                               } catch (err) {}
+			   }
+			   			   
+			   // Get check in data.
+			   var xmlhttp = new XMLHttpRequest(); // TODO: IE5, IE6? ;-)
+			   xmlhttp.onreadystatechange = function() {
+			       if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+				   var response = xmlhttp.responseText.substr(9);
+				   var data = JSON.parse(response);
+				   for(var user in data) {
+				       //console.log(data[user].p_url);
+				       var userData = data[user];
+				       try {
+					   setPushPin(map, userData[time],
+						      userData[p_name], userData[pic], userData[lat], userData[lon]);
+				       } catch (err) {}
+				   }
+			       }
+			   };
+			   xmlhttp.open("GET", "https://www.martinoluca.sb.facebook.com/nearmenow/friends/", true);
+			   xmlhttp.send();			   
+		       }, {scope: "user_events"});
+	    }
+        });
+	/*
+	  FB.api('/me?fields=friends.limit(20).fields(checkins.limit(10).fields(created_time,coordinates),picture)', function(response) {
+	  map = getMap();
+	  // Loop through friends' checkins
+	  for (var i = 0; i < response.friends.data.length; i++) {
+	  try {
+	  for (var j = 0; j < response.friends.data[i].checkins.data; j++) {
+	  var coordinates = response.friends.data[i].checkins.data[j].coordinates;
+	  var createdTime = (new Date(response.friends.data[i].checkins.data[j].created_time)).getTime();
+	  
+	  }
+	  } catch (err) {}
+	  }
+	  });*/
     };
     
     // Load the SDK asynchronously.
